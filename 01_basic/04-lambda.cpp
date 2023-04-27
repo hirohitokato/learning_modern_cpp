e76da1accf7aebf1c2c78d546e7980b17ff19496
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2023-04-28 08:35:29 +0900

    Update comments

--- 01_basic/04-lambda.cpp
+++ 01_basic/04-lambda.cpp
@@ -23,50 +23,60 @@ int main()
     {
         // ラムダ式のいろいろな書き方
 
+        auto f2 = [](double a) -> double
+        {
+            return a * 2
+        };
+
         // 引数を２つ持つ場合
         auto f = [](int a, const char *b)
         {
             cout << a << " & " << b << endl;
-        }; // セミコロンを忘れないように
-        f(193, "Hello");
+        };               // セミコロンを忘れないように
+        f(193, "Hello"); // "193 + Hello"
 
         // 引数２つとint型の返り値を持つ場合
-        auto g = [](int a, int b) -> int
+        auto h = [](int a, int b) -> int
         {
             return a + b;
         };
-        cout << "5 + 8 = " << g(5, 8) << endl;
+        cout << "5 + 8 = " << h(5, 8) << endl; // "5 + 8 = 13"
+
         // この書き方でもOK
         // 返り値の型を推論してくれている
-        auto g2 = [](int a, int b)
+        auto h2 = [](int a, int b)
         {
             return a + b;
         };
-        cout << "4 + 3 = " << g2(4, 3) << endl;
+        cout << "4 + 3 = " << h2(4, 3) << endl; // "4 + 3 = 7"
 
         // (中身の話)↑のラムダ式は、コンパイル時に次のような形で
         // 関数オブジェクトとしてコンパイルされている
-        struct some_G /*名前はコンパイラが勝手に決める*/
+        struct something_H /*名前はコンパイラが勝手に決める*/
         {
             int operator()(int a, int b) const
             {
                 return a + b;
             }
         };
-        some_G g3{};
-        cout << "4 + 3 = " << g3(4, 3) << endl; // 上のg2と同じ結果
-        cout << "type of g2:" << typeid(g2).name() << endl; // macOSだと「Z4mainE3$_4」のように出力される
+        something_H h3{};
+        cout << "4 + 3 = " << h3(4, 3) << endl; // "4 + 3 = 7"。上のg2と同じ結果
+
+        cout << "type of h2:" << typeid(h2).name() << endl;
+        // ↑はwinだと「class <lambda_4fb8879bd4b8231e033ae71dead7e178>」、
+        // macOSだと「Z4mainE3$_4」のように出力される。なのでautoでないと受けられない
     }
     cout << "-------------------------------------" << endl;
     {
         // もっとも小さなラムダ式
-        auto f = []{}; // 引数・返り値が省略されている
-        f(); // 呼べる(何も起きないけれど)
+        auto f = [] {}; // 引数・返り値が省略されている
+        f();            // 呼べる(何も起きないけれど)
 
         // 作成したラムダ式はその場で呼び出せる
-        auto x = [](int a, int b) -> int {
+        auto x = [](int a, int b) -> int
+        {
             return a * b;
-        }(2, 10); // その場で2&10と共に呼んでいる。
+        }(2, 10);                     // その場で2&10と共に呼んでいる。
         cout << "x is " << x << endl; // "x is 20"
     }
     cout << "-------------------------------------" << endl;
@@ -85,14 +95,17 @@ int main()
 
         // 変更したい場合は、変数に&を付けて参照キャプチャする
         // [x](int y){ x+=y; }(5); // コンパイルエラー！xを変更できない
-        [&x](int y){ x+=y; }(5);
-        cout << "current x is " << x << endl; // 47(=42+5)が表示される
+        [&x](int y)
+        { x += y; }(5);
+        cout << "current x is " << x << endl; // "current x is 47"が表示される
 
         // キャプチャする変数は複数まとめて指定できる
         auto y = 81;
         // xをコピーキャプチャ、yを参照キャプチャ
-        auto result2 = [x, &y](int z){ y = x + z; return y; }(5);
+        auto result2 = [x, &y](int z)
+        { y = x + z; return y; }(5);
         cout << "result of `[x, &y](int z){ y = x + z; return y; }(5)` is " << result2 << endl;
+        // ↑ result of `[x, &y](int z){ y = x + z; return y; }(5)` is 52
 
         // オブジェクトのメンバー変数を使用したい場合は、キャプチャに
         // thisを指定する
@@ -112,5 +125,15 @@ int main()
     cout << "-------------------------------------" << endl;
     {
         // 関数引数に渡したり、後で使うようにする場合
+    }
+    cout << "-------------------------------------" << endl;
+    {
+        // キャプチャするときの注意
+        // * キャプチャした変数の寿命に注意
+    }
+    cout << "-------------------------------------" << endl;
+    {
+        // キャプチャしないラムダ式であれば普通の関数ポインタのように使える
+        // (キャプチャしているとコンパイルエラー)
     }
 }
