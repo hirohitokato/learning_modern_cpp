71cc79cadfbe5e04353fa6110ff88791c511b5b9
Hirohito Kato <hkato193@gmail.com>
2023-05-16 21:34:07 +0900

    Update code

--- 01_basic/05-smart_ptr.cpp
+++ 01_basic/05-smart_ptr.cpp
@@ -1,10 +1,17 @@
 // スマートポインタ
 
+#include <memory>
+
+int main() {
+    
+}
 // ポインタを生で使わない
 // * 不正アクセス
 // * 解放忘れ
 // * メモリリーク
 
+// モダンなC++プログラミングではnew/deleteを使わない
+// wxWidgetsは使っているが…
 // unique
 
 // shared
