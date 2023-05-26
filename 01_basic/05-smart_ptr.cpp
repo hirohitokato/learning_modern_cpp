c5d4e96ea67b656711eddeed678cecb699394fb1
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2023-05-26 10:46:19 +0900

    Add comment

--- 01_basic/05-smart_ptr.cpp
+++ 01_basic/05-smart_ptr.cpp
@@ -64,6 +64,11 @@ int main()
     return 0;
 }
 
+void func_with_unique_ptr(std::unique_ptr<int> ptr);
+void func_with_unique_ptr_ref(std::unique_ptr<int> &ptr);
+void func_with_shared_ptr(std::shared_ptr<int> ptr);
+void func_with_shared_ptr_ref(std::shared_ptr<int> &ptr);
+
 //
 // 1. std::unique_ptr
 // 所有者が常に1人であることを保証してくれるスマートポインタ
@@ -73,6 +78,7 @@ void learn_unique_ptr()
     // 作成
     // コンストラクタを使う。ただしできるだけmake_uniqueを使う(C++14～)
     std::unique_ptr<int> x1 = std::make_unique<int>(100);
+    auto x1_ = std::make_unique<int>(193);
     std::unique_ptr<int> x2{new int{193}}; // これも可
     std::unique_ptr<MyClass> obj1 = std::make_unique<MyClass>();
     std::unique_ptr<MyClass> obj2 = std::make_unique<MyClass>(200, 3.14); // コンストラクタへの引数も渡せる
@@ -93,6 +99,8 @@ void learn_unique_ptr()
     // -> : ポインタの要素にアクセスする
     {
         std::cout << x1 << "/" << *x1 << "/" << *x2 << std::endl; // → {アドレス}/100/193
+        *x1 = 4649;                                               // 書き換えられる
+        std::cout << x1 << "/" << *x1 << "/" << *x2 << std::endl; // → {アドレス}/4649/193
         obj1->method();                                           // → Hello smart pointer!
         std::cout << obj2->value << std::endl;                    // → 200
 
@@ -158,6 +166,14 @@ void learn_unique_ptr()
     } // ここで"custom_deleter for 4649 is called."が出力される
       // 注意: release()を途中で呼び中のポインタを取り出すとカスタムデリータも呼ばれなくなる
 
+    // 関数に渡す時の注意
+    {
+        auto obj = std::make_unique<int>(42);
+        // func_with_unique_ptr(obj);            // コンパイルエラー！コピーが発生するので値渡しでは呼べない
+        func_with_unique_ptr_ref(obj);        // これは参照渡しなので呼べる
+        func_with_unique_ptr(std::move(obj)); // これは呼べるが、値を手放しているのでobjは無効になってしまう
+    }
+
     // メリット：
     // * 所有権が１つであることを明確にできる
     // * スコープを外れたら確実に解放してくれる
@@ -168,7 +184,7 @@ void learn_unique_ptr()
 
 //
 // 2. std::shared_ptr
-// 所有者(複数)がいなくなったときにリソース解放を保証してくれるスマートポインタ
+// 所有者が複数。全員手放したときにリソース解放を保証してくれるスマートポインタ
 //
 void learn_shared_ptr()
 {
@@ -176,6 +192,7 @@ void learn_shared_ptr()
     //
     // コンストラクタを使う。ただしできるだけmake_sharedを使う
     std::shared_ptr<int> x1 = std::make_shared<int>(100);
+    auto x1_ = std::make_shared<int>(100);
     std::shared_ptr<int> x2{new int{193}}; // これも可
     std::shared_ptr<MyClass> obj1 = std::make_shared<MyClass>();
     std::shared_ptr<MyClass> obj2 = std::make_shared<MyClass>(200, 3.14); // コンストラクタへの引数も渡せる
@@ -187,9 +204,13 @@ void learn_shared_ptr()
 
     // あとからセットしたい場合はreset()を使う
     {
+        auto *t = new MyClass{15, 1.41};
         std::shared_ptr<MyClass> obj3;
-        obj3.reset(new MyClass{15, 1.41}); // obj3.reset()も可能
-    }
+        obj3.reset(t); // obj3.reset()も可能
+        t->value = 5;
+        std::cout << obj3->value << std::endl; // 5
+
+    } // t(new MyClass{15, 1.41}で作ったやつ)はobj3が破棄してくれる。delete t不要
 
     // 取り扱い: unique_ptrと同じ
     // *  : 中身にアクセスする
@@ -239,11 +260,21 @@ void learn_shared_ptr()
             // owner1とowner3は同じポインタを持つ
             std::cout << "address >> owner1:" << owner1.get()
                       << " owner3:" << owner3.get() << std::endl; // address >> owner1:0xZZZZZZ owner3:0xZZZZZZ
+            owner3->value = 5;
+            std::cout << owner1->value << std::endl;              // owner1でも5が出力される
             std::cout << owner1.use_count() << std::endl;         // 2
         }                                                         // ここでowner3が破棄され、所有者数が -1 される（が、まだ活きている）
 
         std::cout << "address >> owner1:" << owner1.get() << std::endl; // address >> owner1:0xZZZZZZ
-    }                                                                   // ここでowner1も所有者から外れ、保持していたMyClassオブジェクトは破棄される
+
+        // 共有パターン３：
+        {
+            auto ptr = std::make_shared<int>(42);
+            func_with_shared_ptr(ptr);                                  // 関数に値として渡すと参照カウンタ+1
+            func_with_shared_ptr_ref(ptr);                              // 関数に参照として渡すと参照カウンタはそのまま
+            std::cout << "use_count: " << ptr.use_count() << std::endl; // 1
+        }
+    } // ここでowner1も所有者から外れ、保持していたMyClassオブジェクトは破棄される
 
     // 所有権の放棄： unique_ptrにはあるrelease()はない
     {
@@ -298,10 +329,53 @@ void learn_shared_ptr()
         s2->ref = s1;
     } // 本来ここでSampleのデストラクタが呼ばれるはずだが、呼ばれない!!
     // 解消するためには、次に示すstd::weak_ptrを使う必要がある
+
+    // 使い所
+    // - 大きなオブジェクト
+    // - マルチスレッド間で共有
+    // - その他
+    // 関数引数に使ったときに参照カウンタは増える？
+    // - 値として渡す場合は増える。引数で渡すときにコピーコンストラクタが呼ばれるため
+    // - 参照/ポインタとしてスマートポインタを渡すと増えない
+}
+
+void learn_shared_ptr_extra()
+{
+    // unique_ptrからshared_ptrにする
+    // shared_ptrからunique_ptrはしないほうが良い（設計の問題）
+
+    // １つのポインタに対して異なるグループのshared_ptrを作らない
 }
 
 void learn_weak_ptr()
 {
+    // 問題
+    // 循環参照せざるを得ない場合にどうする？
+    // 所有者の都合で破棄したときに、参照している人は破棄されたことをどうやって確認する？
+
+    shared_ptr<aircraft> wingMan = pMaverick->myWingMan.lock();
+    if (wingMan)
+    {
+        cout << wingMan->m_flyCount << endl;
+    }
 }
 
 // scoped/autoは使わない
+
+void func_with_unique_ptr(std::unique_ptr<int> ptr)
+{
+}
+
+void func_with_unique_ptr_ref(std::unique_ptr<int> &ptr)
+{
+}
+
+void func_with_shared_ptr(std::shared_ptr<int> ptr)
+{
+    std::cout << "shared_ptr in function: " << ptr.use_count() << std::endl; // +1した値が出力される
+}
+
+void func_with_shared_ptr_ref(std::shared_ptr<int> &ptr)
+{
+    std::cout << "shared_ptr& in function: " << ptr.use_count() << std::endl; // 参照なので+1されない
+}
