def3c568b6d5953a1844adfeb6aff4d926ec707b
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2023-06-15 17:24:32 +0900

    Update document

--- 02-advanced/01-threads-2.cpp
+++ 02-advanced/01-threads-2.cpp
@@ -6,5 +6,39 @@
 // C++ 11以前で使われていたOS/環境依存のライブラリは、必要なとき以外は使わない。
 
 #include <iostream>
+#include <string>
 #include <thread>
+#include <mutex>
 
+namespace
+{
+    std::mutex _printMutex;
+}
+
+void print_threadsafe(const std::string &msg)
+{
+    // std::coutはスレッドセーフではないため、他のスレッドとで出力が混じることがあります
+    // ここではmutexを使って排他的な出力処理を実現しています。
+    std::lock_guard lock{_printMutex};
+    std::cout << msg << std::endl;
+}
+
+void my_function()
+{
+    print_threadsafe("  | Runs on thread.");
+}
+
+int main()
+{
+    // スレッドの作成と実行開始
+    std::thread my_thread{my_function};
+
+    // 【注意!】
+    // 下の出力処理はスレッドセーフではないため、他のスレッドとで出力が混じることがあります
+    print_threadsafe("Output on main thread.");
+
+    // スレッド終了の待ち合わせ
+    my_thread.join();
+
+    return 0;
+}
