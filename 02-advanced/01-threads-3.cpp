378219cb86802e7ef8bb6a710bb933e9ca157e42
Hirohito Kato <hkato193@gmail.com>
2023-07-19 01:20:02 +0900

    Update code

--- 02-advanced/01-threads-3.cpp
+++ 02-advanced/01-threads-3.cpp
@@ -1,7 +1,66 @@
 #include <thread>
+#include <mutex>
+#include <iostream>
+#include <vector>
 
 std::mutex m;
+std::recursive_mutex rm;
 
-int main() {
-    
+int shared_value = 0;
+
+void function_1a()
+{
+    m.lock();
+
+    if (shared_value > 1)
+    {
+        return;
+    }
+    shared_value++;
+    std::cout << "function_1a: " << shared_value << std::endl;
+    m.unlock();
+}
+
+void function_1b()
+{
+    // このスコープの中でmによる排他を実現する
+    std::lock_guard lock{m};
+
+    if (shared_value > 1)
+    {
+        return; // 途中で抜けてもmをunlockしてくれる
+    }
+    shared_value++; // 共有している情報に安全にアクセスできる
+    std::cout << "function_1b: " << shared_value << std::endl;
+}
+
+void func2()
+{
+    std::lock_guard lock1{rm};
+    std::lock_guard lock2{rm}; // 同一スレッドからであれば複数回ロック可能
+}
+
+int main()
+{
+    {
+        std::vector<std::thread> threads;
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            std::thread t1{function_1a};
+            std::thread t2{function_1b};
+            threads.push_back(std::move(t1));
+            threads.push_back(std::move(t2));
+        }
+
+        for (auto &thread : threads)
+        {
+            thread.join();
+        }
+    }
+
+    {
+    }
+
+    return 0;
 }
--- 02-advanced/01-threads-3.md
+++ 02-advanced/01-threads-3.md
@@ -7,8 +7,8 @@
 |---|---|---|
 |`std::mutex`||ロック/非ロックで排他領域を作り出せる|
 |`std::recursive_mutex`||同じスレッドであれば何回でも`lock()`できる|
-|`std::shared_mutex`|C++17|
-|`std::shared_timed_mutex`|C++14|
+|`std::shared_mutex`|C++17|リーダーライターロックを提供
+|`std::shared_timed_mutex`|C++14|タイマー付のリーダーライターロックを提供
 |`std::binary_semaphore`|C++20|バイナリセマフォ。`counting_semaphore<1>`相当|
 |`std::counting_semaphore`|C++20|計数セマフォ|
 
@@ -16,7 +16,7 @@
 
 # ロック獲得/解放処理の便利メソッド
 
-RAII、あるいは[Scoped Locking Pattern](https://www.dre.vanderbilt.edu/~schmidt/PDF/ScopedLocking.pdf)と呼ばれる方法で、スコープの寿命にロック獲得/解放を同期させるためのメソッドが用意されている。
+RAIIあるいは[Scoped Locking Pattern](https://www.dre.vanderbilt.edu/~schmidt/PDF/ScopedLocking.pdf)と呼ばれる方法で、スコープの寿命にロック獲得/解放を同期させるための管理メソッド群が用意されている。
 
 |型|ver.|説明|
 |---|---|---|
