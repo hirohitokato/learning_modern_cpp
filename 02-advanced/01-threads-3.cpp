c0e50472cb45763cf03f5530fc7eeb5b99ef2dfc
Hirohito Kato <hkato193@gmail.com>
2023-07-19 01:31:35 +0900

    Update code

--- 02-advanced/01-threads-3.cpp
+++ 02-advanced/01-threads-3.cpp
@@ -11,12 +11,13 @@ int shared_value = 0;
 void function_1a()
 {
     m.lock();
+    shared_value++;
 
-    if (shared_value > 1)
+    if (shared_value > 5)
     {
+        m.unlock();
         return;
     }
-    shared_value++;
     std::cout << "function_1a: " << shared_value << std::endl;
     m.unlock();
 }
@@ -25,24 +26,33 @@ void function_1b()
 {
     // このスコープの中でmによる排他を実現する
     std::lock_guard lock{m};
+    shared_value++; // 共有している情報に安全にアクセスできる
 
-    if (shared_value > 1)
+    if (shared_value > 5)
     {
         return; // 途中で抜けてもmをunlockしてくれる
     }
-    shared_value++; // 共有している情報に安全にアクセスできる
     std::cout << "function_1b: " << shared_value << std::endl;
 }
 
-void func2()
+void function_2b()
 {
     std::lock_guard lock1{rm};
     std::lock_guard lock2{rm}; // 同一スレッドからであれば複数回ロック可能
+    std::cout << std::this_thread::get_id() << ": " << __func__ << std::endl;
+}
+
+void function_2a()
+{
+    std::lock_guard lock{rm};
+    std::cout << std::this_thread::get_id() << ": " << __func__ << std::endl;
+    function_2b(); // 同じrmをロックする関数を呼んでも安全
 }
 
 int main()
 {
     {
+        // 単純なロック
         std::vector<std::thread> threads;
 
         for (size_t i = 0; i < 10; i++)
@@ -60,6 +70,12 @@ int main()
     }
 
     {
+        // 再帰ロックを使うと、ロックを同じスレッドで連続して獲得できる
+        std::thread t1{function_2a};
+        std::thread t2{function_2a};
+
+        t2.join();
+        t1.join();
     }
 
     return 0;
