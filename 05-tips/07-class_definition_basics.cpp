f1ee3cbcbe656b8520053401d78f7144686d1b39
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2023-09-22 08:43:26 +0900

    Update code

--- 05-tips/07-class_definition_basics.cpp
+++ 05-tips/07-class_definition_basics.cpp
@@ -59,6 +59,11 @@ namespace some_namespace
         // ※例外を出力しない場合は`noexcept`を書く(パフォーマンスに影響)
         int publicMethod(const std::string &input, std::string &output) noexcept;
 
+        // 継承してもらうことを狙ったメソッドにはvirtualキーワードを付けて
+        // 仮想関数にしておく。（付けないと常に親クラスのメソッドが呼ばれてしまう）
+        // （親クラスでのインスタンス化を禁止したい場合は virtual ～() = 0;と純粋仮想関数にしておく)
+        virtual void overridableMethod(int arg1, const std::string &str) {}
+
         // 実行してもメンバ変数の状態を変更しない関数はconstを付ける(パフォーマンスに影響)
         // ※constを付ける場合はスレッドセーフになるように設計すること
         virtual std::string toString() const;
@@ -72,8 +77,17 @@ namespace some_namespace
     {
     public:
         SubClass(/* args */);
-        virtual ~SubClass(); // さらに継承される可能性がある場合はvirtualをつける
+        virtual ~SubClass(); // さらに継承される可能性がある場合は必ずvirtualをつける
 
         // 親クラスでdeleteしたものはサブクラスでもdeleteされる
+
+        // 親クラスの仮想関数(virtualキーワードを付けた関数)をオーバーライドするときは
+        // `override`キーワードを付けると安全
+        void overridableMethod(int arg1, const std::string &str) override {}
     };
+
+    // これ以上継承させたくないときは`final`を付ける
+    class SubClass2 final : public BaseClass
+    {
+    }
 }
