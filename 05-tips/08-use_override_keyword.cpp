8ca0099d4cc1d5a90f65e6127c34ea53088124f6
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2023-09-27 08:52:06 +0900

    Update code

--- 05-tips/08-use_override_keyword.cpp
+++ 05-tips/08-use_override_keyword.cpp
@@ -7,7 +7,7 @@ class Base
 public:
     virtual void f()
     {
-        std::cout << "Base class default behaviour\n";
+        std::cout << "Base class\n";
     }
 };
 
@@ -18,7 +18,7 @@ namespace Bad
     public:
         virtual void f() const
         {
-            std::cout << "Derived class overridden behaviour\n";
+            std::cout << "Derived class\n";
         }
     };
 }
@@ -28,19 +28,33 @@ namespace Good
     class Derived : public Base
     {
     public:
-        // virtual void f() const override
+        // virtual void f() const override と書くとコンパイルエラーにしてくれる
         virtual void f() override
         {
-            std::cout << "Derived class overridden behaviour\n";
+            std::cout << "Derived class (overridden)\n";
         }
     };
 }
 
 int main()
 {
-    auto ptr = std::make_unique<Bad::Derived>();
+    {
+        Bad::Derived obj{};
+        obj.f(); // → Derived class
 
-    ptr->f();
+        std::unique_ptr<Base> ptr = std::make_unique<Bad::Derived>();
+        // Base *ptr = new Bad::Derived(); でもOK
+        ptr->f(); // → Base class (!?)
+    }
+
+    {
+        Good::Derived obj{};
+        obj.f(); // → Derived class (overridden)
+
+        std::unique_ptr<Base> ptr = std::make_unique<Good::Derived>();
+        // Base *ptr = new Bad::Derived(); でもOK
+        ptr->f(); // → Derived class (overridden) (yay!)
+    }
 
     return 0;
 }
